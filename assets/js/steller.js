ff08145e4c6898ce9d1d38861ab0be119a0a830b
calvinnickholaa <nickholascalvin1@gmail.com>
2023-06-06 10:16:12 +0700

    add page animation

--- assets/js/steller.js
+++ assets/js/steller.js
@@ -13,20 +13,40 @@
 */
 
 // smooth scroll
-$(document).ready(function(){
-	$(".nav-link").on('click', function(event) {
+$(document).ready(function () {
+  $(".nav-link").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
 
-    	if (this.hash !== "") {
+      var hash = this.hash;
 
-			event.preventDefault();
-
-			var hash = this.hash;
-
-			$('html, body').animate({
-				scrollTop: $(hash).offset().top
-			}, 700, function(){
-				window.location.hash = hash;
-			});
-      	} 
-    });
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top
+        },
+        700,
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
 });
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
